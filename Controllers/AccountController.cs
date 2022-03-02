395cc7fec4c922f800842553f9e8c72d97b58304
MohammadAsgharian <m.asgharian@gmail.com>
2022-03-02 22:13:42 +0330

    master

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -4,6 +4,9 @@ using maghsadAPI.Models.Dto;
 using System.Threading.Tasks;
 using maghsadAPI.Models.Identity;
 using maghsadAPI.Infrastructure;
+using System.Security.Claims;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 
 namespace maghsadAPI.Controllers
 {
@@ -21,21 +24,35 @@ namespace maghsadAPI.Controllers
             _tokenService = tokenService;
         }
 
-        [HttpPost("login")]
-        public async Task<ActionResult<UserDto>> Login()
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
-           // var user = await _userManager.FindByEmailAsync(loginDto.Email);
-            // if(user == null) return Unauthorized();
-            
-            // var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            var email = HttpContext.User?.Claims?.FirstOrDefault(x=> x.Type == ClaimTypes.Email).Value;
+            var user =await _userManager.FindByEmailAsync(email);
 
-            // if(!result.Succeeded) return Unauthorized();
-            AppUser user = new AppUser{
-                Email = "m.asgharian@gmail.com",
-                Id = "1",
-                UserName ="Mass2007"
-
+             return new UserDto
+            {
+                Email = user.Email,
+                Token = _tokenService.CreateToken(user),
+                Username =  user.UserName
             };
+        }
+
+
+
+
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+           var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if(user == null) return Unauthorized();
+            
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if(!result.Succeeded) return Unauthorized();
+         
 
 
             return new UserDto
--- Models/Identity/IdentityServiceExtensions.cs
+++ Models/Identity/IdentityServiceExtensions.cs
@@ -28,6 +28,7 @@ namespace maghsadAPI.Models.Identity
                         IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"])),
                         ValidIssuer= config["Token:Issuer"],
                         ValidateIssuer = true,
+                        ValidateAudience = false
                     };
                 });
             
