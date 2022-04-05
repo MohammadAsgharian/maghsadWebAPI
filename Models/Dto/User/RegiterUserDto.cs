119ef2db47ae90c09f127cb6af6032cae125e638
MohammadAsgharian <m.asgharian@gmail.com>
2022-04-05 08:23:50 +0430

    master

--- Models/Dto/User/RegiterUserDto.cs
+++ Models/Dto/User/RegiterUserDto.cs
@@ -2,9 +2,6 @@ namespace maghsadAPI.Models.Dto
 {
     public class RegiterUserDto
     {
-        public string UserName{get; set;}
-        public string FirstName{get; set;}
-        public string LastName{get; set;}
         public string Email { get; set; }
         public string Password { get; set; }
     }
